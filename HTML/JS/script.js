3f7c27f2bdeda11acd34b804f8b2734d4fa8260d
Martina <32537447+mmdt1990@users.noreply.github.com>
2018-03-19 22:43:36 +0100

    Add files via upload

---
+++ HTML/JS/script.js
@@ -0,0 +1,38 @@
+//Menu - burger
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
+//Audio
+
+
+function play() {
+    var audio = document.getElementById('audio');
+    if (audio.paused) {
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    } else {
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+}
